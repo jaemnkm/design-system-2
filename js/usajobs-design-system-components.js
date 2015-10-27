4a6279e308eafce4de6d34203dc25c42d7a8209a
Matthew Dingee <matthew.dingee@opm.gov>
2015-10-27 11:59:47 -0400

    Pushing the nav closer to completion:  * some styling on the inner menus  * handling desktop Corrected a positioning problem in the nav. [Amends #4] Correcting a JS compile problem in the Gruntfile.

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -502,3 +502,34 @@ $nav.on('nav.close', function(event, opts) {
     opts.target.attr('aria-expanded', 'false');
   });
 });
+
+$nav.on('nav.menu.toggle', function(event, opts) {
+  var $parent = opts.el.parent(),
+    parent_state = $parent.attr('data-state'),
+    $menu = $parent.find('[role="menu"]'),
+    $sibling = $parent.siblings().find('[aria-expanded="true"]');
+
+  event.preventDefault();
+
+  $nav.trigger('nav.menu.slide-close', { parent: $sibling.parent(), menu: $sibling });
+
+  if (parent_state === 'is-closed') {
+    $nav.trigger('nav.menu.slide-open', { parent: $parent, menu: $menu });
+  } else if (parent_state === 'is-open') {
+    $nav.trigger('nav.menu.slide-close', { parent: $parent, menu: $menu });
+  }
+});
+
+$nav.on('nav.menu.slide-open', function(event, opts) {
+  opts.menu.slideDown("fast", function () {
+    opts.parent.attr('data-state', 'is-open');
+    opts.menu.attr('aria-expanded', 'true');
+  });
+});
+
+$nav.on('nav.menu.slide-close', function(event, opts) {
+  opts.menu.slideUp("fast", function () {
+    opts.parent.attr('data-state', 'is-closed');
+    opts.menu.attr('aria-expanded', 'false');
+  });
+});
