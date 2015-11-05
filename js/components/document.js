27bd3d0ecbdbd4937578afb58c8f917076b87e5e
Matthew Dingee <matthew.dingee@opm.gov>
2015-11-05 11:58:04 -0500

    Revised placeholder and thumbnail document component. [Amends #19]

--- js/components/document.js
+++ js/components/document.js
@@ -6,6 +6,7 @@ $doc.on('click', '[data-behavior]', function (event) {
   var $el = $(this),
     $object = $el.closest('[data-object="document"]'),
     behavior = $el.attr('data-behavior'),
+    state = $object.attr('data-state'),
     $target = $object.find($el.attr('data-target'));
 
   event.preventDefault();
@@ -13,15 +14,23 @@ $doc.on('click', '[data-behavior]', function (event) {
 
   // Each behavior attached to the element should be triggered
   $.each(behavior.split(' '), function (idx, action) {
-    $el.trigger(action, { el: $el, object: $object, target: $target });
+    $el.trigger(action, { el: $el, object: $object, state: state, target: $target });
   });
 });
 
 $doc.on('document.toggle', function(event, opts) {
+  var $input = opts.object.find('#' + opts.target.attr('for'));
+
   event.preventDefault();
 
-  // This event is fired by the figure and simply clicks the checkbox
-  // which we're leveraging for state
-
+  // This event is fired by the figure and clicks the checkbox
   opts.target.trigger('click');
+
+  if (opts.state !== 'is-disabled') {
+    if ($input.is(':checked')) {
+      opts.object.attr('data-state', 'is-selected');
+    } else {
+      opts.object.attr('data-state', 'is-selectable');
+    }
+  }
 });
