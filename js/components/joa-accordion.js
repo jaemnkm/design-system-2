ae22260a02f8d265b64eb3d7e8f2492e70bd04b3
Matthew Dingee <matthew.dingee@opm.gov>
2016-02-18 09:47:01 -0500

    Fixing drawer hiding/showing bug. [Amends #57]

--- js/components/joa-accordion.js
+++ js/components/joa-accordion.js
@@ -5,20 +5,24 @@ var $joa_accordion = $('[data-object="joa-accordion"]'),
   // Open the accordion at the appropriate viewport size
   openAccordions = function () {
     var viewport_width = parseInt($(window).innerWidth(), 10),
-      $accordion;
+      $accordion,
+      $trigger;
 
     $.each($joa_accordion, function (index, accordion) {
       $accordion = $(accordion);
+      $trigger = $accordion.siblings('.usajobs-joa-accordion-trigger');
 
       if (viewport_width > 599) {
         if ($accordion.hasClass('is-open-at-M')) {
           $accordion.attr('aria-hidden', 'false');
+          $trigger.removeAttr('aria-controls');
         }
       }
 
       if (viewport_width > 991) {
         if ($accordion.hasClass('is-open-at-L')) {
           $accordion.attr('aria-hidden', 'false');
+          $trigger.removeAttr('aria-controls');
         }
       }
     });
