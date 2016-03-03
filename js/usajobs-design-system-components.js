e5c98a3271918ebd5ad9d615e425fc6d0e215ea6
Matthew Dingee <matthew.dingee@opm.gov>
2016-03-03 17:58:39 -0500

    Slight enhacement to the visual style of the autocomplete. [Amends #44]

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -395,9 +395,22 @@ usajSrc = function (request, response) {
   });
 };
 
+// Portions of this code came from http://jsfiddle.net/fozylet/kTAMm/
 $('#search-location').autocomplete({
   source: usajSrc,
   minLength: 3,
+  open: function (e, ui) {
+    var acData = $(this).data('ui-autocomplete');
+    acData
+      .menu
+      .element
+      .find('li')
+      .each(function () {
+        var me = $(this);
+        var keywords = acData.term.split(' ').join('|');
+        me.html(me.text().replace(new RegExp("(" + keywords + ")", "gi"), '<strong>$1</strong>'));
+      });
+  },
   select: function (event, ui) {
     var selectedObj = ui.item;
 
