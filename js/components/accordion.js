f6a13780a8d65261553b73dc83f17b5da77c5d21
Matthew Dingee <matthew.dingee@opm.gov>
2016-04-07 21:21:29 -0400

    Breaking out the 18F accordion.js.

---
+++ js/components/accordion.js
@@ -0,0 +1,71 @@
+/**
+ * Accordion
+ *
+ * An accordion component.
+ *
+ * @param {jQuery} $el A jQuery html element to turn into an accordion.
+ */
+function Accordion($el) {
+  var self = this;
+  this.$root = $el;
+  this.$root.on('click', 'button', function(ev) {
+    var expanded = JSON.parse($(this).attr('aria-expanded'));
+    ev.preventDefault();
+    self.hideAll();
+    if (!expanded) {
+      self.show($(this));
+    }
+  });
+}
+
+Accordion.prototype.$ = function(selector) {
+  return this.$root.find(selector);
+};
+
+Accordion.prototype.hide = function($button) {
+  var selector = $button.attr('aria-controls'),
+      $content = this.$('#' + selector);
+
+  $button.attr('aria-expanded', false);
+  $content.attr('aria-hidden', true);
+};
+
+Accordion.prototype.show = function($button) {
+  var selector = $button.attr('aria-controls'),
+      $content = this.$('#' + selector);
+
+  $button.attr('aria-expanded', true);
+  $content.attr('aria-hidden', false);
+
+  /*
+  Backing this out as we don't always want this behavior
+  $('html, body').animate({
+    scrollTop: $content.offset().top
+  });
+  */
+};
+
+Accordion.prototype.hideAll = function() {
+  var self = this;
+  this.$('button').each(function() {
+    self.hide($(this));
+  });
+};
+
+/**
+ * accordion
+ *
+ * Initialize a new Accordion component.
+ *
+ * @param {jQuery} $el A jQuery html element to turn into an accordion.
+ */
+function accordion($el) {
+  return new Accordion($el);
+}
+
+$(function() {
+  $('[class^=usa-accordion]').each(function() {
+    accordion($(this));
+  });
+});
+
