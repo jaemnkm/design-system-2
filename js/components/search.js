8820dc34e1ffa10fe09806d8dc05625bfe6a41c6
Matthew Dingee <matthew.dingee@opm.gov>
2016-07-19 21:22:55 -0400

    Splits autocomplete out into a new search.js component file so that the Help Center may use it independently of nav.

---
+++ js/components/search.js
@@ -0,0 +1,46 @@
+// Search
+
+// Search autocomplete locations
+var acHostUrl = 'https://ac.usajobs.gov/acwd',
+usajSrc = function (request, response) {
+  $.ajax({
+    url: acHostUrl,
+    dataType: 'jsonp',
+    crossdomain: true,
+    cache: true,
+    jsonpCallback: 'usaj151067976',
+    data: { t: request.term },
+    success: function (data) {
+      response(data);
+    }
+  });
+};
+
+// Portions of this code came from http://jsfiddle.net/fozylet/kTAMm/
+$('#search-location').autocomplete({
+  source: usajSrc,
+  minLength: 3,
+  open: function (e, ui) {
+    var acData = $(this).data('ui-autocomplete');
+    acData
+      .menu
+      .element
+      .find('li')
+      .each(function () {
+        var me = $(this);
+        var keywords = acData.term.split(' ').join('|');
+        me.html(me.text().replace(new RegExp("(" + keywords + ")", "gi"), '<strong>$1</strong>'));
+      });
+  },
+  select: function (event, ui) {
+    var selectedObj = ui.item;
+
+    $('#search-location').val(selectedObj.label);
+    $('#AutoCompleteSelected').val('true');
+    return false;
+  },
+  search: function () {
+    //wipe out values on new searches or when user selects one but changes their mind!
+    $('#AutoCompleteSelected').val('false');
+  }
+});
