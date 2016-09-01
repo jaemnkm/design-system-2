2a07a849d7dfffa257d9566532dbc6ddf5d0f2c8
Matthew Dingee <matthew.dingee@opm.gov>
2016-09-01 14:36:06 -0400

    Adds Share component to JOA actions.

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -260,12 +260,11 @@ $footer.on('footer.close', function(event, opts) {
 // JOA Actions
 var $joa_actions = $('[data-object="joa-actions"]');
 
-$joa_actions.on('click', '[data-behavior]', function (event) {
+$joa_actions.on('click', '[data-behavior]', function () {
   var $el = $(this),
     $object = $el.closest('[data-object="joa-actions"]'),
     behavior = $el.attr('data-behavior');
 
-  event.preventDefault();
   $el.blur(); // Removes focus
 
   // Each behavior attached to the element should be triggered
@@ -281,12 +280,36 @@ $joa_actions.on('joa-actions.toggle-save', function(event, opts) {
 
   event.preventDefault();
 
-  if (saved_state === "is-saved") {
+  if (saved_state === 'is-saved') {
     opts.el.text('Save');
     opts.el.attr('data-state', 'is-unsaved');
   } else {
     opts.el.text('Saved');
     opts.el.attr('data-state', 'is-saved');
+  }
+});
+
+$joa_actions.on('joa-actions.toggle-share', function(event, opts) {
+  var target_id = opts.el.attr('id'),
+    $target = opts.object.find('[aria-labeledby="' + target_id + '"]'),
+    state = $target.attr('aria-hidden');
+
+  if (state === 'true') {
+    $target.attr('aria-hidden', false);
+  } else {
+    $target.attr('aria-hidden', true);
+  }
+});
+
+$joa_actions.on('joa-actions.close-share', function(event, opts) {
+  var target_id = opts.el.attr('data-target'),
+    $target = opts.object.find(target_id),
+    state = $target.attr('aria-hidden');
+
+  if (state === 'true') {
+    $target.attr('aria-hidden', false);
+  } else {
+    $target.attr('aria-hidden', true);
   }
 });
 
