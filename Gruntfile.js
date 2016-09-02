c3af05decbda80d208866253bb0986f45dd54b0e
Matthew Dingee <matthew.dingee@opm.gov>
2016-09-02 10:01:24 -0400

    Fixing up package.json according to lint. Adding jshint reporter to gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
     // shell commands for use in Grunt tasks
     shell: {
       jekyllBuild: {
-        command: "bundle exec jekyll build"
+        command: 'bundle exec jekyll build'
       },
       jekyllServe: {
         command: "bundle exec jekyll serve --baseurl ''"
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
     autoprefixer: {
       options: {
         map: true, // Use and update the sourcemap
-        browsers: ["last 3 versions", "> 2% in US"]
+        browsers: ['last 3 versions', '> 2% in US']
       },
       project_css: {
         expand: true,
@@ -132,6 +132,9 @@ module.exports = function(grunt) {
     },
     jshint: {
       jshintrc: true,
+      options: {
+        reporter: require('jshint-stylish')
+      },
       all: [
         'Gruntfile.js',
         'js/**/*.js',
