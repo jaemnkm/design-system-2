67268a5bbf6770476dec01e52bebdbe73ba99704
Matthew Dingee <matthew.dingee@opm.gov>
2016-12-01 14:27:44 -0500

    Updates no results text, pagination component, and search controls component.

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -924,7 +924,7 @@ var $targets = $('[rel~=tooltip]'),
   pos_left,
   pos_top;
 
-$targets.bind('mouseenter', function () {
+$targets.on('mouseenter', function () {
   $target  = $(this);
   tip     = $target.attr('title');
   $tooltip = $('<div class="usajobs-tooltip" id="tooltip"></div>');
@@ -987,8 +987,8 @@ $targets.bind('mouseenter', function () {
     $target.attr( 'title', tip );
   };
 
-  $target.bind('mouseleave', removeTooltip);
-  $tooltip.bind('click', removeTooltip );
+  $target.on('mouseleave', removeTooltip);
+  $tooltip.on('click', removeTooltip );
 });
 
 
