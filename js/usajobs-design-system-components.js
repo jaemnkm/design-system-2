66a7b676f7cacfedc9249e58adbe643cdbd9713c
Matthew Dingee <matthew.dingee@opm.gov>
2017-06-29 16:08:53 -0400

    Adds word-break: break-all to joa-section p elements to prevent long links from breaking through the edges of a section [Amends #107]

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -590,6 +590,50 @@ $(window).scroll($.throttle(250, function () {
   $fixed_nav.toggleClass('is-fixed', $(this).scrollTop() > $fixed_nav.height());
 }));
 
+function onScroll() {
+  var scrollPos = $(document).scrollTop();
+
+  $('#menu-center a').each(function () {
+      var currLink = $(this),
+        refElement = $(currLink.attr('href'));
+
+      if (refElement.position().top <= scrollPos &&
+          refElement.position().top + refElement.height() > scrollPos) {
+        $nav_secondary.find('ul li a').removeClass('active');
+        currLink.addClass('active');
+      }
+      else{
+        currLink.removeClass('active');
+      }
+  });
+}
+
+$(document).ready(function () {
+  $(document).on('scroll', onScroll);
+
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off('scroll');
+
+      $('a').each(function () {
+        $(this).removeClass('active');
+      });
+
+      $(this).addClass('active');
+
+      var target = this.hash,
+        $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on('scroll', onScroll);
+      });
+    });
+});
+
 // Navigation (top-level-nav)
 
 var $nav = $('[data-object="nav"]'),
