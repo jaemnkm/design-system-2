23c96f946e2fe6f92b120a43eb5be78bb04c5d62
Matthew Dingee <matthew.dingee@opm.gov>
2017-06-30 11:22:37 -0400

    Adds print version of job announcement. Adds some missing content to the reasonable accommodation section.

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -827,20 +827,22 @@ $notification.on('notification.delayed-close', function(event, opts) {
 var $reveal = $('[data-object="reveal"]'),
   $reveal_more = $('[data-object="reveal-more"]'),
   hideReadMoreContent = function () {
-    $reveal_more.readmore({
-      speed: 500,
-      collapsedHeight: 100,
-      moreLink: '<a class="usajobs-read-more__link" href="#">Read more</a>',
-      lessLink: '<a class="usajobs-read-more__link" href="#">Close</a>',
-      heightMargin: 17, // Add the class 'transitioning' before toggling begins.
-      beforeToggle: function(trigger, element) {
-        element.addClass('transitioning');
-      },
-      // Remove the 'transitioning' class when toggling completes.
-      afterToggle: function(trigger, element) {
-        element.removeClass('transitioning');
-      }
-    });
+    if (! $('body').hasClass('is-print-version')) {
+      $reveal_more.readmore({
+        speed: 500,
+        collapsedHeight: 100,
+        moreLink: '<a class="usajobs-read-more__link" href="#">Read more</a>',
+        lessLink: '<a class="usajobs-read-more__link" href="#">Close</a>',
+        heightMargin: 17, // Add the class 'transitioning' before toggling begins.
+        beforeToggle: function(trigger, element) {
+          element.addClass('transitioning');
+        },
+        // Remove the 'transitioning' class when toggling completes.
+        afterToggle: function(trigger, element) {
+          element.removeClass('transitioning');
+        }
+      });
+    }
   };
 
 hideReadMoreContent();
