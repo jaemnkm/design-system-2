b0107830930cac08de13b548e76058b54d5c5856
Matthew Dingee <matthew.dingee@opm.gov>
2017-10-06 16:25:21 -0400

    Corrects some JS build issues. Fixes some of the code accordions. Adds the Open Opps logo. [Amends #112]

--- js/components/joa-sections.js
+++ js/components/joa-sections.js
@@ -1,12 +1,29 @@
 // JOA Sections
 
-var $reveal_more = $('[data-object="reveal-more"]');
+var $reveal_more = $('[data-object="reveal-more"]'),
+  hideReadMoreContent = function () {
+    if (! $('body').hasClass('is-print-version')) {
+      $reveal_more.readmore({
+        speed: 500,
+        collapsedHeight: 100,
+        moreLink: '<a class="usajobs-read-more__link" href="#">Read more</a>',
+        lessLink: '<a class="usajobs-read-more__link" href="#">Close</a>',
+        heightMargin: 17, // Add the class 'transitioning' before toggling begins.
+        beforeToggle: function(trigger, element) {
+          element.addClass('transitioning');
+        },
+        // Remove the 'transitioning' class when toggling completes.
+        afterToggle: function(trigger, element) {
+          element.removeClass('transitioning');
+        }
+      });
+    }
+  };
 
 $(window).resize($.throttle(250, function() {
   var width = window.innerWidth;
 
   if (width > 599) {
-    // This function lives in reveal.js
     if (! $reveal_more.attr('style')) {
       hideReadMoreContent();
     }
--- js/usajobs-design-system-documentation.js
+++ js/usajobs-design-system-documentation.js
@@ -17,7 +17,8 @@ $(function(){
       event.preventDefault();
     });
   }
-  handleDisabledLinks()
+
+  handleDisabledLinks();
 
   // TODO restructure function so the use of "this" makes sense.
   var generateCodeSnippets = function(content, previewBox) {
@@ -26,27 +27,27 @@ $(function(){
 
     this.parseCode = function(previewBox) {
       var sampleCode = $('<div>');
-      $(sampleCode).html($(previewBox).html())
+      $(sampleCode).html($(previewBox).html());
       $(sampleCode).find('.is-peripheral').remove();
       return sampleCode;
-    }
+    };
 
     this.render = function(previewBox, sampleCode) {
-
-      var sampleCodeBox = $(''+
-        '<div class="usa-accordion-bordered usa-code-sample">' +
-          '<ul class="usa-unstyled-list">' +
-            '<li>' +
-              '<button class="usa-accordion-button usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-0">Code</button>' +
-              '<div id="collapsible-0" aria-hidden="true" class="usa-accordion-content">' +
-                '<pre><code class="language-markup"></code></pre>' +
-              '</div>' +
-            '</li>' +
-          '</ul>' +
-        '</div>');
+      var previewBoxName = $(previewBox).attr('id') + '-code',
+        sampleCodeBox = $(''+
+          '<div class="usa-accordion-bordered usa-code-sample">' +
+            '<ul class="usa-unstyled-list">' +
+              '<li>' +
+                '<button class="usa-accordion-button usa-button-unstyled" aria-expanded="false" aria-controls="' + previewBoxName + '">Code</button>' +
+                '<div id="'+ previewBoxName + '" aria-hidden="true" class="usa-accordion-content">' +
+                  '<pre><code class="language-markup"></code></pre>' +
+                '</div>' +
+              '</li>' +
+            '</ul>' +
+          '</div>');
       $(sampleCodeBox).find('code').text($(sampleCode).html());
       $(previewBox).after(sampleCodeBox);
-    }
+    };
 
     $(content).find(previewBox).each(function(index, previewBox) {
 
@@ -54,8 +55,7 @@ $(function(){
       self.render(previewBox, sampleCode);
 
     });
-
-  }
+  };
 
   generateCodeSnippets('.main-content', '.preview');
 
@@ -82,7 +82,7 @@ var calculateAnchorPosition = function (hash) {
 
   //anchor should now align with first item inside nav
   return anchor.offset().top - topOffset;
-}
+};
 
 
 /* When user lands on a page with a hash in the url
