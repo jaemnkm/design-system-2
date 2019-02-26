25a4a4bdcc125153db3c2a453df2e27bc1e9ea3e
Matthew Dingee <matthew.dingee@opm.gov>
2019-02-26 14:02:08 -0500

    Creates an A and B of the pilot JOA for a usability test tomorrow that incorporates warnings requested by OPM Policy. [Amends #167]

--- js/usajobs-design-system-components.js
+++ js/usajobs-design-system-components.js
@@ -373,65 +373,69 @@ $modal.on('modal.close', function(event, opts) {
 // Secondary Nav
 
 var $nav_secondary = $('[data-object="nav-secondary"]'),
-  $fixed_nav = $('.is-fixed-nav'),
-
+  $fixed_nav = $(".is-fixed-nav"),
+  $nav_inpage = $('[data-behavior="nav-inpage"]'),
   // The majority of this function is used courtesty of
   // Brad Frost's responsive design patterns:
   // https://bradfrost.github.io/this-is-responsive/patterns.html
   // Specifically, the Priority+ Alt pattern.
-  navSecondaryToggleMenuItems = function () {
+  navSecondaryToggleMenuItems = function() {
     var nav_width = 0,
-      $menu = $nav_secondary.find('.usajobs-nav-secondary__menu'),
-      $more_menu = $menu.find('.more'),
-      $more_container = $nav_secondary.find($nav_secondary.attr('data-target')),
+      $menu = $nav_secondary.find(".usajobs-nav-secondary__menu"),
+      $more_menu = $menu.find(".more"),
+      $more_container = $nav_secondary.find($nav_secondary.attr("data-target")),
       more_width = $more_menu.outerWidth(true),
       available_space,
       last_item,
       first_more_el;
 
-    $menu.find('> li:not(.more)').each(function () {
-      nav_width += $(this).outerWidth( true );
+    $menu.find("> li:not(.more)").each(function() {
+      nav_width += $(this).outerWidth(true);
     });
 
     available_space = $nav_secondary.outerWidth(true) - more_width;
 
     if (nav_width > available_space) {
-      $menu.each(function () {
-        last_item = $menu.find('> li:not(.more)').last();
-        last_item.attr('data-width', last_item.outerWidth(true));
+      $menu.each(function() {
+        last_item = $menu.find("> li:not(.more)").last();
+        last_item.attr("data-width", last_item.outerWidth(true));
         last_item.prependTo($more_container);
         navSecondaryToggleMenuItems();
       });
     } else {
-      first_more_el = $more_menu.find('li').first();
+      first_more_el = $more_menu.find("li").first();
 
-      if (nav_width + first_more_el.data('width') < available_space) {
+      if (nav_width + first_more_el.data("width") < available_space) {
         first_more_el.insertBefore($more_menu);
       }
     }
 
-    if ($more_menu.find('li').length > 0) {
-      $more_menu.css('display','inline-block');
+    if ($more_menu.find("li").length > 0) {
+      $more_menu.css("display", "inline-block");
     } else {
-      $more_menu.css('display','none');
+      $more_menu.css("display", "none");
     }
   },
-  updateActiveNav = function () {
+  updateActiveNav = function() {
     var scroll_position = $(document).scrollTop(),
       fixed_nav = $fixed_nav.length > 0,
-      $links = $nav_secondary.find('[data-behavior="nav-secondary.scroll-to-top nav-secondary.make-active"]');
+      $links = $nav_secondary.find(
+        '[data-behavior="nav-secondary.scroll-to-top nav-secondary.make-active"]'
+      );
 
     if (fixed_nav) {
-      $links.each(function () {
+      $links.each(function() {
         var $link = $(this),
-          $section = $($link.attr('href'));
+          $section = $($link.attr("href"));
 
-        if ($section.position().top <= scroll_position &&
-            $section.position().top + $section.height() > scroll_position) {
-          $links.removeClass('is-active');
-          $link.addClass('is-active');
+        if (
+          $section.position().top <= scroll_position &&
+          $section.position().top + $section.height() > scroll_position
+        ) {
+          $links.removeClass("is-active");
+          $link.addClass("is-active");
         } else {
-          $link.removeClass('is-active');
+          $link.removeClass("is-active");
         }
       });
     }
@@ -442,82 +446,119 @@ if ($nav_secondary !== undefined && $nav_secondary.length > 0) {
   navSecondaryToggleMenuItems();
 }
 
-// Toggle Secondary Nav items on resize
-$(window).resize( $.throttle( 250, function() {
-  if ($nav_secondary !== undefined && $nav_secondary.length > 0) {
-    navSecondaryToggleMenuItems();
-  }
-}));
+// NOTE: This block is only for a usability test on 2/27
+if ($nav_inpage !== undefined && $nav_inpage.length > 0) {
+  // This is a hardcoded behavior that just mimics
+  // clicking on a secondary nav item
+  $nav_inpage.on("click", function(event) {
+    var $el = $(this),
+      $object = $(document).find('[data-object="nav-secondary"]'),
+      $nav_link = $object.find('[href="' + $el.attr("href") + '"]');
 
-$nav_secondary.on('click', '[data-behavior]', function (event) {
+    event.preventDefault();
+    $el.blur(); // Removes focus
+
+    $nav_link.trigger("click");
+  });
+}
+
+// Toggle Secondary Nav items on resize
+$(window).resize(
+  $.throttle(250, function() {
+    if ($nav_secondary !== undefined && $nav_secondary.length > 0) {
+      navSecondaryToggleMenuItems();
+    }
+  })
+);
+
+$nav_secondary.on("click", "[data-behavior]", function(event) {
   var $el = $(this),
     $object = $el.closest('[data-object="nav-secondary"]'),
-    behavior = $el.attr('data-behavior'),
-    $target = $object.find($object.attr('data-target')),
-    state = $target.attr('data-state');
+    behavior = $el.attr("data-behavior"),
+    $target = $object.find($object.attr("data-target")),
+    state = $target.attr("data-state");
 
   event.preventDefault();
   $el.blur(); // Removes focus
 
   // Each behavior attached to the element should be triggered
-  $.each(behavior.split(' '), function (idx, action) {
-    $el.trigger(action, { el: $el, object: $object, state: state, target: $target });
+  $.each(behavior.split(" "), function(idx, action) {
+    $el.trigger(action, {
+      el: $el,
+      object: $object,
+      state: state,
+      target: $target
+    });
   });
 });
 
-$nav_secondary.on('nav-secondary.toggle', function(event, opts) {
+$nav_secondary.on("nav-secondary.toggle", function(event, opts) {
   event.preventDefault();
 
-  if (opts.state === 'is-closed') {
-    opts.object.trigger('nav-secondary.open', opts);
-  } else if (opts.state === 'is-open') {
-    opts.object.trigger('nav-secondary.close', opts);
+  if (opts.state === "is-closed") {
+    opts.object.trigger("nav-secondary.open", opts);
+  } else if (opts.state === "is-open") {
+    opts.object.trigger("nav-secondary.close", opts);
   }
 });
 
-$nav_secondary.on('nav-secondary.open', function(event, opts) {
-  opts.target.attr('data-state', 'is-open');
+$nav_secondary.on("nav-secondary.open", function(event, opts) {
+  opts.target.attr("data-state", "is-open");
 });
 
-$nav_secondary.on('nav-secondary.close', function(event, opts) {
-  opts.target.attr('data-state', 'is-closed');
+$nav_secondary.on("nav-secondary.close", function(event, opts) {
+  opts.target.attr("data-state", "is-closed");
 });
 
-$nav_secondary.on('nav-secondary.make-active', function(event, opts) {
-  var $siblings = opts.object.find('.is-active');
+$nav_secondary.on("nav-secondary.make-active", function(event, opts) {
+  var $siblings = opts.object.find(".is-active");
 
-  $siblings.removeClass('is-active');
-  opts.el.addClass('is-active');
+  $siblings.removeClass("is-active");
+  opts.el.addClass("is-active");
 });
 
-$nav_secondary.on('nav-secondary.scroll-to-top', function(event, opts) {
-  var target = opts.el.attr('href'),
-    $target = $('body').find(target),
+$nav_secondary.on("nav-secondary.scroll-to-top", function(event, opts) {
+  var target = opts.el.attr("href"),
+    $target = $("body").find(target),
     offset_height = 0;
 
-  $(document).off('scroll');
+  $(document).off("scroll");
 
-  if (opts.object.attr('data-behavior') === 'is-fixed-nav') {
+  if (opts.object.hasClass("is-fixed-nav")) {
     if ($(window).scrollTop() < opts.object.height()) {
-      // Determining this height has proven to be illogical. it's not the offeset.top value. Why not?
-      offset_height = (opts.object.height() * 2.5) + 15;
+      // Determining this height has proven to be illogical. It's not the offeset.top value. Why not?
+      offset_height = opts.object.height() * 2.5;
+      window.console.log("offset_height ADJUSTED" + offset_height);
     } else {
-      offset_height = opts.object.height();
+      offset_height = opts.object.height() + 15;
+      window.console.log("offset_height" + offset_height);
     }
   }
 
-  $('html, body').stop().animate({
-    'scrollTop': $target.offset().top - offset_height
-  }, 500, 'swing', function () {
-    window.location.hash = target;
-  });
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $target.offset().top - offset_height
+      },
+      500,
+      "swing",
+      function() {
+        history.pushState({}, "", target);
+      }
+    );
 });
 
 // Fixed secondary nav on scroll
-$(window).scroll($.throttle(250, function () {
-  $fixed_nav.toggleClass('is-fixed', $(this).scrollTop() > $fixed_nav.height());
-  updateActiveNav();
-}));
+$(window).scroll(
+  $.throttle(250, function() {
+    $fixed_nav.toggleClass(
+      "is-fixed",
+      $(this).scrollTop() > $fixed_nav.height()
+    );
+    updateActiveNav();
+  })
+);
 
 // Navigation (top-level-nav)
 
